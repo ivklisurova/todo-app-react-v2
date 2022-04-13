949d951929d3aded5f5a1c23dd9e7445bcb5c27d
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-04-13 11:50:33 +0300

    add ToDoItem.jsx to split the code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,45 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
+import ToDoItem from "./ToDoItem";
 
 function App() {
-  const [list, setList] = useState([]);
-  const [item, setItem] = useState("");
+    const [list, setList] = useState([]);
+    const [item, setItem] = useState("");
 
-  const listItems = list.map((item, index) => <li key={index}>{item}</li>);
+    // const listItems = list.map((item, index) => <li key={index}>{item}</li>);
 
-  function handleChange(event) {
-    const newItem = event.target.value;
+    function handleChange(event) {
+        const newItem = event.target.value;
 
-    setItem(newItem);
-  }
+        setItem(newItem);
+    }
 
-  function handleClick() {
-    !(item.length === 0 || !item.trim()) &&
-      setList((prevItems) => {
-        return [...prevItems, item];
-      });
+    function handleClick() {
+        !(item.length === 0 || !item.trim()) &&
+        setList((prevItems) => {
+            return [...prevItems, item];
+        });
 
-    setItem("");
-  }
+        setItem("");
+    }
 
-  return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input type="text" onChange={handleChange} name="task" value={item} />
-        <button type="submit" onClick={handleClick}>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>{listItems}</ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="heading">
+                <h1>To-Do List</h1>
+            </div>
+            <div className="form">
+                <input type="text" onChange={handleChange} name="task" value={item}/>
+                <button type="submit" onClick={handleClick}>
+                    <span>Add</span>
+                </button>
+            </div>
+            <div>
+                <ul>
+                    {list.map((item, index) => <ToDoItem text={item}/>)}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
